7eb90ae2048f9fe10786a8905eadd615f1a22396
shirish2601 <you@example.com>
2023-05-06 20:41:34 +0530

    added alerts

--- public/js/login.js
+++ public/js/login.js
@@ -4,6 +4,8 @@ btn.addEventListener("click", async (e) => {
   e.stopImmediatePropagation();
   const email = document.querySelector(".emailval").value;
   const password = document.querySelector(".passwdval").value;
+  const emailContainer = document.querySelector(".passwdval");
+  const passwordContainer = document.querySelector(".passwdval");
   try {
     const response = await fetch("http://localhost:5500/login", {
       method: "POST",
@@ -17,14 +19,41 @@ btn.addEventListener("click", async (e) => {
     });
 
     if (!response.ok) {
-      email.value = "";
-      password.value = "";
-      const alertHTML = ``;
-      document.body.insertAdjacentHTML;
+      const error = await response.json();
+      document.querySelector(".emailval").value = "";
+      document.querySelector(".passwdval").value = "";
+      const alertHTML = `<div class="alert alert-error top-right-corner">
+      <p>${error.message}</p>
+    </div>`;
+      $(function () {
+        $("body").append(alertHTML);
+
+        $(".alert").css("opacity", 1);
+
+        setTimeout(function () {
+          $(".alert").fadeOut(1000, function () {
+            $(this).remove();
+          });
+        }, 2000);
+      });
     } else {
-      email.value = "";
-      password.value = "";
-      window.location.href = "/me/";
+      passwordContainer.value = "";
+      emailContainer.value = "";
+      const successAlertHTML = `<div class="alert alert-success top-right-corner">
+      <p>Login Successful</p>
+    </div>`;
+      $(function () {
+        $("body").append(successAlertHTML);
+
+        $(".alert").css("opacity", 1);
+
+        setTimeout(function () {
+          $(".alert").fadeOut(100, function () {
+            $(this).remove();
+            window.location.href = "/me/"; // Redirect after the alert has been removed
+          });
+        }, 200);
+      });
     }
   } catch (err) {
     console.log(err);
--- public/js/register.js
+++ public/js/register.js
@@ -2,15 +2,16 @@ const button = document.querySelector(".btn");
 button.addEventListener("click", async (e) => {
   e.preventDefault();
   e.stopImmediatePropagation();
-  let userName = document.querySelector(".nameval").value;
-  let userPassword = document.querySelector(".passwdval").value;
-  let userEmail = document.querySelector(".emailval").value;
+  let userName = document.querySelector(".nameval");
+  let userPassword = document.querySelector(".passwdval");
+  let userEmail = document.querySelector(".emailval");
+  userName = userName.value;
+  userPassword = userPassword.value;
+  userEmail = userEmail.value;
 
-  // remove whitespaces
   userName = userName.trim();
   userPassword = userPassword.trim();
   userEmail = userEmail.trim();
-  console.log(userName, userPassword, userEmail);
   try {
     const response = await fetch("http://localhost:5500/register", {
       method: "POST",
@@ -23,10 +24,42 @@ button.addEventListener("click", async (e) => {
         email: userEmail,
       }),
     });
-
+    document.querySelector(".nameval").value = "";
+    document.querySelector(".passwdval").value = "";
+    document.querySelector(".emailval").value = "";
     if (!response.ok) {
-      throw new Error("Signup Failed!");
+      const error = await response.json();
+      const alertHTML = `<div class="alert alert-error top-right-corner">
+      <p>${error.message}</p>
+    </div>`;
+
+      $(function () {
+        $("body").append(alertHTML);
+
+        $(".alert").css("opacity", 1);
+
+        setTimeout(function () {
+          $(".alert").fadeOut(1000, function () {
+            $(this).remove();
+          });
+        }, 2000);
+      });
     } else {
+      const successAlertHTML = `<div class="alert alert-success top-right-corner">
+      <p>Registration Successful</p>
+    </div>`;
+      $(function () {
+        $("body").append(successAlertHTML);
+
+        $(".alert").css("opacity", 1);
+
+        setTimeout(function () {
+          $(".alert").fadeOut(300, function () {
+            $(this).remove();
+            window.location.href = "/me/";
+          });
+        }, 400);
+      });
       window.location.href = "/me/";
     }
   } catch (err) {
