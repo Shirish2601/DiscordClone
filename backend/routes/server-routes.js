1ff6933f1379ce3d59c5bdc8668228cbd9fe61d1
shirish2601 <you@example.com>
2023-05-24 13:13:35 +0530

    fixed absolute path

--- backend/routes/server-routes.js
+++ backend/routes/server-routes.js
@@ -1,15 +1,20 @@
 const express = require("express");
 
 const router = express.Router();
+const path = require("path");
+const VIEWS_PATH = path.join(__dirname, "../../", "views");
 
 router.get("/getpopup", (req, res, next) => {
   res.set("Content-Type", "text/html");
-  res.sendFile("F:\\College\\Discord Clone\\views\\popup.ejs");
+  res.sendFile(path.join(VIEWS_PATH, "/popup.ejs"));
+  // res.sendFile("F:\\College\\Discord Clone\\views\\popup.ejs");
+  // res.render(path.join(VIEWS_PATH, "/index"));
 });
 
 router.get("/getpopupcreatechannel", (req, res, next) => {
   res.set("Content-Type", "text/html");
-  res.sendFile("F:\\College\\Discord Clone\\views\\popupcreatechannel.ejs");
+  res.sendFile(path.join(VIEWS_PATH, "/popupcreatechannel.ejs"));
+  // res.sendFile("F:\\College\\Discord Clone\\views\\popupcreatechannel.ejs");
 });
 
 module.exports = router;
